a942c244a407f76315552c277db3e26592708815
meriambs <bensalah.meriem0@gmail.com>
2021-05-08 04:10:46 +0100

    partie front

--- client/src/App.js
+++ client/src/App.js
@@ -4,34 +4,32 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useRoutes,
+  Switch
+ 
 } from "react-router-dom";
 
-import routes from './routes'
-// import Navigation from './Compoment/Navigation';
-// import Landing from './Landing';
-// import Register from "./Compoment/Authentification/Register";
-// import Login from './Compoment/Authentification/Login';
+ import Navigation from './Compoment/Navigation';
+ import Landing from './Landing';
+ import Register from "./Compoment/Authentification/Register";
+ import Login from './Compoment/Authentification/Login';
 
 function App() {
-  const routing = useRoutes(routes);
+ 
   return (
-  //   <Router>
-  //   <div className="App">
-  //    <Navigation/>
-  //    <Route exact path="/" component={Landing}/>
-  //  <section className="container">
-  //  <Switch>
-  //  <Route exact path="/register" component={Register}/>
-  //  <Route path="/login" component={Login}/>
-  //  </Switch>
-  //  </section>
+     <Router>
+     <div className="App">
+      <Navigation/>
+      <Route exact path="/" component={Landing}/>
+    <section className="container">
+    <Switch>
+    <Route exact path="/register" component={Register}/>
+    <Route path="/login" component={Login}/>
+    </Switch>
+    </section>
   
-  //   </div>
-  //   // </Router>
-  <div className="App">
-  {routing}
-  </div>
+     </div>
+   </Router>
+
   );
 }
 
