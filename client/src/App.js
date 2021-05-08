daa9d1564c0c6825c7179feb28a51c5b3a7027db
meriambs <bensalah.meriem0@gmail.com>
2021-05-08 01:58:22 +0100

    partie routes avec useRoutes

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,37 @@
+import React from 'react';
 import './index.css';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Link
+  useRoutes,
 } from "react-router-dom";
-import Navigation from './Compoment/Navigation';
-import Landing from './Landing';
-import Register from "./Compoment/Authentification/Register";
-import Login from './Compoment/Authentification/Login';
+
+import routes from './routes'
+// import Navigation from './Compoment/Navigation';
+// import Landing from './Landing';
+// import Register from "./Compoment/Authentification/Register";
+// import Login from './Compoment/Authentification/Login';
 
 function App() {
+  const routing = useRoutes(routes);
   return (
-    <Router>
-    <div className="App">
-     <Navigation/>
-     <Route exact path="/" component={Landing}/>
-   <section className="container">
-   <Switch>
-   <Route exact path="/register" component={Register}/>
-   <Route path="/login" component={Login}/>
-   </Switch>
-   </section>
+  //   <Router>
+  //   <div className="App">
+  //    <Navigation/>
+  //    <Route exact path="/" component={Landing}/>
+  //  <section className="container">
+  //  <Switch>
+  //  <Route exact path="/register" component={Register}/>
+  //  <Route path="/login" component={Login}/>
+  //  </Switch>
+  //  </section>
   
-    </div>
-    </Router>
+  //   </div>
+  //   // </Router>
+  <div className="App">
+  {routing}
+  </div>
   );
 }
 
